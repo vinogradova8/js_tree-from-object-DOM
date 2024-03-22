5bd0640c9c4c12ab3672bf110a03e874cf188ec9
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-03-22 12:43:33 +0200

    make build

---
+++ index.c6d15257.js
@@ -0,0 +1,2 @@
+!function e(n,t){let r=document.createElement("ul");for(let l of(n.append(r),Object.entries(t))){let n=document.createElement("li");l.length>0&&(n.innerText=l[0],r.append(n),e(n,l[1]))}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.c6d15257.js.map
